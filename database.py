1a9bb66795c14a4c5a6f07567461072fe361491b
Ahatha <richahatha@gmail.com>
2026-07-22 11:25:14 +0530

    new change

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+import os
 from student import Student
 
 
 class StudentDatabase:
-    def __init__(self):
+    def __init__( self):
         self.students = []
 
     def add_student(self, student_id, name, age):
